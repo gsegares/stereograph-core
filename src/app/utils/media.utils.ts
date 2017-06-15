faafb10617deb60807d452bb407a386cc3f1dc18
geannina <geannina@zarmada.com>
2017-06-15 15:38:54 -0700

    Adding utils and postFormData method

--- src/app/utils/media.utils.ts
+++ src/app/utils/media.utils.ts
@@ -13,4 +13,14 @@ export class Media {
       'audio/ogg'
     ].indexOf(type) > -1;
   }
+
+  public static dataUrlToBlob(dataUrl: string, mimeType: string): Blob {
+    const binary = atob(dataUrl.split(',')[1]);
+    let array = [];
+    for (let i = 0; i < binary.length; i++) {
+      array.push(binary.charCodeAt(i));
+    }
+
+    return new Blob([new Uint8Array(array)], {type: mimeType});
+  }
 }
